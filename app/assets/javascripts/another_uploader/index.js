8e6f2e5c79240f6dc2736e1431dab871326184ee
Isaac Betesh <iybetesh@gmail.com>
2012-11-15 10:44:37 -0500

    created manifest JS and CSS files for engine that include uploadify assets and configured dummy app to include assets from easy_rails_authentication and another_uploader

---
+++ app/assets/javascripts/another_uploader/index.js
@@ -0,0 +1,5 @@
+//= require jquery
+//= require jquery-ui
+//= require best_in_place
+//= require uploadify
+//= require_tree ../
--- test/dummy/app/assets/javascripts/application.js
+++ test/dummy/app/assets/javascripts/application.js
@@ -13,3 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+//= require another_uploader
+//= require easy_rails_authentication
