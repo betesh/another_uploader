1727ccdecc15d890984d434f5281c6d19e10738d
Isaac Betesh <iybetesh@gmail.com>
2012-11-24 18:26:00 -0500

    added requirement for imagemagick to be installed

--- lib/another_uploader/engine.rb
+++ lib/another_uploader/engine.rb
@@ -1,4 +1,12 @@
 module AnotherUploader
   class Engine < ::Rails::Engine
+    initializer 'image magick must be installed' do
+      convert = `which convert`
+      if "" == convert
+        puts "imagemagick must be installed for the tests to run."
+        puts "Please execute `sudo apt-get install imagemagick` and then try again"
+        exit
+      end
+    end
   end
 end
